3016dd2554f00f8a4cc2be0337373dc747b86698
Yash <yashpandey1106@gmail.com>
2024-03-14 17:48:31 +0530

    update the post feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,14 +19,23 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @post.increment!(:views)
     @comments = @post.comments.fetch_post_comments
+  end
 
+  def turbo_frame_posts
+  end
+
+  def all_users_posts
+    @user = User.find_by_id(params[:id])
+    @posts = Post.where(user_id: @user.id)
+    render :turbo_frame_posts
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:description, medias: [])
+    params.require(:post).permit(:caption, medias: [])
   end
 
 end
